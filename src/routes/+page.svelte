43a7c15a9cb2d04c8f09caf4a8ca858c5a71de52
Irhas <muhammadirhasalh@gmail.com>
2023-07-12 06:49:06 +0700

    Add drawer content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,50 @@
-<script>
-	import { AppBar } from '@skeletonlabs/skeleton';
-	import { LightSwitch } from '@skeletonlabs/skeleton';
-	import { Drawer, drawerStore } from '@skeletonlabs/skeleton';
+<script lang="ts">
+	import { AppBar, LightSwitch, Drawer, drawerStore } from '@skeletonlabs/skeleton';
+	import type { DrawerSettings } from '@skeletonlabs/skeleton';
 	import Menu from 'svelte-material-icons/Menu.svelte';
 	import Github from 'svelte-material-icons/Github.svelte';
+	import Linkedin from 'svelte-material-icons/Linkedin.svelte';
+	import Instagram from 'svelte-material-icons/Instagram.svelte';
+	import Android from 'svelte-material-icons/Android.svelte';
 
 	let href = 'https://github.com/mirhasalh/mirhasalhxyz';
 	let size = '1.5rem';
 	let viewBox = '0 0 24 24';
+
+	const drawerSettings: DrawerSettings = {
+		id: 'main-drawer',
+		width: 'w-auto'
+	};
 </script>
 
-<Drawer />
+<Drawer>
+	{#if $drawerStore.id === 'main-drawer'}
+		<a href="https://flutter.mirhasalh.xyz/#/" class="btn variant-filled m-4" data-sveltekit-preload-data="hover" target="_blank">
+			<Android {size} {viewBox} />
+		</a>
+		<br>
+		<a {href} class="btn variant-filled m-4" data-sveltekit-preload-data="hover" target="_blank">
+			<Github {size} {viewBox} />
+		</a>
+		<br>
+		<a href="https://www.instagram.com/irhasdev/" class="btn variant-filled m-4" data-sveltekit-preload-data="hover" target="_blank">
+			<Instagram {size} {viewBox} />
+		</a>
+		<br>
+		<a href="https://www.linkedin.com/in/irhas-a-914681230" class="btn variant-filled m-4" data-sveltekit-preload-data="hover" target="_blank">
+			<Linkedin {size} {viewBox} />
+		</a>
+	{:else}
+		<div>...</div>
+	{/if}
+</Drawer>
 <AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
 	<svelte:fragment slot="lead">
-		<button type="button" class="btn-icon bg-initial" on:click={() => drawerStore.open()}>
+		<button
+			type="button"
+			class="btn-icon bg-initial"
+			on:click={() => drawerStore.open(drawerSettings)}
+		>
 			<Menu {size} {viewBox} />
 		</button>
 	</svelte:fragment>
